a69239ed17960e5c2e5e4c483d2e3560b3bf6cdf
Sam Welch <samwelch1819@gmail.com>
2024-01-02 09:24:50 +0530

    enhanced tests

--- tests/addDefaultValues.test.js
+++ tests/addDefaultValues.test.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
 import { addDefaultValues } from "../utilities/addDefaultValues.js";
-import tests from "./test-add-default-values.json";
+import testData from "./test-add-default-values.json";
 
-tests.forEach((eachTest, index) => {
-  test(`Running test ${index + 1}: ${eachTest.name}`, () => {
-    const result = addDefaultValues(eachTest.schema, eachTest.document);
-    const expected = JSON.parse(JSON.stringify(eachTest.expected));
-    expect(result).toEqual(expected);
+testData.tests.forEach((eachTest, index) => {
+  test(`Running test ${index + 1}: ${eachTest.description}`, () => {
+    const result = addDefaultValues(testData.schema, eachTest.instance);
+    expect(result).toEqual(eachTest.expected);
   });
 });
--- tests/removeExtraProperties.test.js
+++ tests/removeExtraProperties.test.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
 import { removeExtraProperties } from "../utilities/removeExtraProperties.js";
-import tests from "./test-remove-extra-properties.json";
+import testData from "./test-remove-extra-properties.json";
 
-tests.forEach((eachTest, index) => {
-  test(`Running test ${index + 1}: ${eachTest.name}`, () => {
-    const result = removeExtraProperties(eachTest.schema, eachTest.document);
-    const expected = JSON.parse(JSON.stringify(eachTest.expected));
-    expect(result).toEqual(expected);
+testData.tests.forEach((eachTest, index) => {
+  test(`Running test ${index + 1}: ${eachTest.description}`, () => {
+    const result = removeExtraProperties(testData.schema, eachTest.instance);
+    expect(result).toEqual(eachTest.expected);
   });
 });
