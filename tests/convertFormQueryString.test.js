35cc094161f5332d58c7e3e81a26a8f9df6c02fb
Sam Welch <samwelch1819@gmail.com>
2024-01-02 22:20:17 +0530

    test function added for convertFormQueryString utility fn

---
+++ tests/convertFormQueryString.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import { convertFormQueryString } from "../utilities/convertFormQueryString";
+import testData from "./test-convert-form-query-string.json";
+
+testData.tests.forEach((eachTest, index) => {
+  test(`Running test ${index + 1}: ${eachTest.description}`, () => {
+    const result = convertFormQueryString(
+      testData.schema,
+      eachTest.queryString
+    );
+    expect(result).toEqual(eachTest.expected);
+  });
+});
