bca11df385477b7df3e288a26b422fd339733be0
Sam Welch <samwelch1819@gmail.com>
2024-01-11 22:41:59 +0530

    throw 'Invalid instance' message when the instance is invalid

--- utilities/addDefaultValues/addDefaultValues.js
+++ utilities/addDefaultValues/addDefaultValues.js
@@ -6,16 +6,16 @@ import {
 
 export const addDefaults = async (schema, document) => {
   let parsedSchema;
+  const result = await parseAndValidateSchema(schema, document);
 
-  try {
-    parsedSchema = await parseAndValidateSchema(schema, document);
-  } catch (error) {
-    parsedSchema = "Invalid Json Schema";
-  }
-
-  if (parsedSchema === "Invalid Json Schema") {
+  if (result === "Invalid Json Schema") {
     return "Invalid Json Schema";
+  } else if (!result.isValid) {
+    return "Invalid Instance";
+  } else {
+    parsedSchema = result.schema;
   }
+
   let updatedDocument;
   try {
     updatedDocument = addDefaultsToObject(parsedSchema, document);
@@ -29,9 +29,12 @@ export const addDefaults = async (schema, document) => {
 const parseAndValidateSchema = async (schema, document) => {
   try {
     registerSchema(schema);
-    await validate(schema.$id, document);
+    const result = await validate(schema.$id, document);
+
+    const isValid = result.valid;
     unregisterSchema(schema.$id);
-    return schema;
+
+    return { schema, isValid };
   } catch (error) {
     console.error("Error validating the Schema", { cause: error });
     return "Invalid Json Schema";
@@ -66,35 +69,53 @@ const addDefaultsToObject = (schema, obj) => {
         }
       }
     }
-  } else if (schema.type === "array" || schema.prefixItems) {
+  } else if (schema.type === "array" || schema.prefixItems || schema.items) {
     return addDefaultsToArray(schema, obj);
   } else {
     return schema.default;
   }
   return obj;
 };
-const addDefaultsToArray = (schema, resultantArr) => {
-  const prefixItemsArr = schema.prefixItems;
 
+const addPrefixDefaults = (prefixItemsArr, instanceArr) => {
   for (let i = 0; i < prefixItemsArr.length; i++) {
-    if (resultantArr[i] === undefined || resultantArr[i] === null) {
+    if (instanceArr[i] === undefined || instanceArr[i] === null) {
       if (prefixItemsArr[i].default) {
-        resultantArr[i] = prefixItemsArr[i].default;
-      } else return resultantArr;
-    } else if (prefixItemsArr[i].type === "object") {
-      resultantArr[i] = {};
-      addDefaultsToObject(prefixItemsArr[i], resultantArr[i]);
+        instanceArr[i] = prefixItemsArr[i].default;
+      } else return instanceArr;
+    } else if (
+      prefixItemsArr[i].type === "object" ||
+      prefixItemsArr[i].properties
+    ) {
+      instanceArr[i] = {};
+      addDefaultsToObject(prefixItemsArr[i], instanceArr[i]);
     } else if (
       prefixItemsArr[i].type === "array" ||
-      prefixItemsArr[i].items ||
-      prefixItemsArr[i].prefixItems
+      prefixItemsArr[i].prefixItems ||
+      prefixItemsArr[i].items
     ) {
-      resultantArr[i] ??= [];
-      if (Array.isArray(prefixItemsArr[i].prefixItems)) {
-        addDefaultsToArray(prefixItemsArr[i], resultantArr[i]);
-      }
+      // instanceArr[i] ??= [];
+      addDefaultsToArray(prefixItemsArr[i], instanceArr[i]);
+    } else {
+      addDefaultsToObject(prefixItemsArr[i], instanceArr);
     }
   }
+  return instanceArr;
+};
+
+const addDefaultsToArray = (schema, arr) => {
+  let resultantArr = arr;
+  if (schema.prefixItems) {
+    resultantArr = addPrefixDefaults(schema.prefixItems, arr);
+  }
+
+  if (schema.items) {
+    const prefixArrLength = schema.prefixItems ? schema.prefixItems.length : 0;
+    for (let i = prefixArrLength; i < arr.length; i++) {
+      addDefaultsToObject(schema.items, arr[i]);
+    }
+  }
+
   return resultantArr;
 };
 
@@ -114,16 +135,18 @@ const schema = {
   $id: "https://example.com/4",
   $schema: "https://json-schema.org/draft/2020-12/schema",
   type: "array",
-  prefixItems: [true],
-  items: {
-    type: "object",
-    properties: {
-      foo: { default: 42 },
-    },
-  },
+  prefixItems: [{ type: "string", default: "Agni" }],
+  // prefixItems: [true],
+  // items: {
+  //   type: "object",
+  //   properties: {
+  //     foo: { default: 42 },
+  //   },
+  // },
 };
 
 const document = [{}, {}, { foo: true }, {}];
 
 const result = await addDefaults(schema, document);
+// // console.log(schema);
 console.log(result);
